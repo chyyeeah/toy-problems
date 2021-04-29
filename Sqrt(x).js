87f0fbee19f143a7206dba73b60feade76bb977b
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-28 20:53:03 -0700

    O(n)

---
+++ Sqrt(x).js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+/*
+Runtime: 96 ms, faster than 69.58% of JavaScript online submissions for Sqrt(x).
+Memory Usage: 40.2 MB, less than 14.45% of JavaScript online submissions for Sqrt(x).
+*/
+var mySqrt = function(x) {
+  return Math.floor(x ** .5);
+};
