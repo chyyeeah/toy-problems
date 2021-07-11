242c63b84c07293c1e9846d63ee6365f5213d051
chyyeeah <wilsoncheah5@gmail.com>
2021-07-10 18:35:16 -0700

    time: O(n), space: O(1) Moore's Voting Algo

--- majorityElement.js
+++ majorityElement.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+/** time: O(n), space: O(n) */
 var majorityElement = function (nums) {
   const hash = new Map();
   const mid = Math.floor(nums.length / 2);
@@ -14,6 +15,26 @@ var majorityElement = function (nums) {
     if (newCount > mid) return num;
     hash.set(num, newCount);
   }
+};
+
+/** time: O(n), space: O(1) */
+var majorityElement = function (nums) {
+  let majCount = 0,
+    majIndex = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    if (num === nums[majIndex]) {
+      majCount++;
+    } else {
+      majCount--;
+    }
+
+    if (majCount === 0) {
+      majIndex = i;
+    }
+  }
+  return nums[majIndex];
 };
 
 console.log(majorityElement([3, 2, 3]));
