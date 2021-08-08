04e8e3af4de0d2a71aaf86d0dcdac0653a80b61b
Wilson Cheah <wilsoncheah5@gmail.com>
2021-08-08 10:23:14 -0700

    O(n) 2 pointers. used a dummy node for edge cases(list with only 1 node or removing the head of the list)

---
+++ second_try/removeNthFromEnd.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0, head),
+    slow = dummy,
+    fast = head;
+
+  while (n > 0 && fast !== null) {
+    fast = fast.next;
+    n--;
+  }
+  if (n > 0) return null;
+
+  while (fast !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  slow.next = slow.next.next;
+  return dummy.next;
+};
