7a3a350d062a3cb4a4c01fbea24ae1e71ef3a3f0
chyyeeah <wilsoncheah5@gmail.com>
2021-08-12 23:42:43 -0700

    incorrect naive solution. BFS

---
+++ CourseScheduleII.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function (numCourses, prerequisites) {
+  const root = new Set(),
+    d = {};
+  let max = -1;
+
+  prerequisites.forEach((p) => {
+    const [c, prereq] = p;
+    root.add(prereq);
+    if (!d[prereq]) d[prereq] = new Set();
+    d[prereq].add(c);
+  });
+
+  prerequisites.forEach((p) => {
+    const [c, prereq] = p;
+    root.delete(c);
+    max = Math.max(max, c);
+  });
+
+  if (root.size === 0 && prerequisites.length > 0) return [];
+
+  const visited = new Set(),
+    queue = [],
+    result = [];
+
+  root.forEach((c) => {
+    queue.push(c);
+  });
+
+  while (queue.length > 0) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const c = queue.shift();
+      if (!visited.has(c)) {
+        result.push(c);
+        if (d[c]) {
+          d[c].forEach((futureClass) => {
+            queue.push(futureClass);
+          });
+        }
+      }
+      visited.add(c);
+    }
+  }
+
+  if (result.length < numCourses) {
+    const diff = numCourses - result.length;
+    for (let i = max + 1; i <= max + diff; i++) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
+
+// console.log(findOrder(2, [[1, 0]]));
+// console.log(findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]]));
+console.log(findOrder(1, []));
+// console.log(findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2], [0, 3]]));
+// console.log(findOrder(3, [[1,0]]));
