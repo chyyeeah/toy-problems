c125135eae156e118e0ce7952ef540325ab423c0
Wilson Cheah <wilsoncheah5@gmail.com>
2021-08-17 15:26:59 -0700

    cleaned up recursive solution

--- findStrobogrammatic.js
+++ findStrobogrammatic.js
@@ -54,50 +54,77 @@
  * @param {number} n
  * @return {string[]}
  */
+// var findStrobogrammatic = function (n) {
+//   const valid = { 0: '0', 1: '1', 6: '9', 8: '8', 9: '6' },
+//     mid = Math.floor((n - 1) / 2),
+//     result = [];
+
+//   if (n === 1) result.push('0');
+
+//   const recurse = (idx, str) => {
+//     if (str[0] === '0') return;
+//     if (idx >= n) return result.push(str);
+
+//     if (idx < mid) {
+//       const keys = Object.keys(valid);
+//       for (let i = 0; i < keys.length; i++) {
+//         recurse(idx + 1, str + keys[i]);
+//       }
+//     } else if (idx === mid) {
+//       if (n % 2 === 0) {
+//         const keys = Object.keys(valid);
+//         for (let i = 0; i < keys.length; i++) {
+//           recurse(idx + 1, str + keys[i]);
+//         }
+//       } else {
+//         const middleKeys = ['0', '1', '8'];
+//         for (let i = 0; i < middleKeys.length; i++) {
+//           recurse(idx + 1, str + middleKeys[i]);
+//         }
+//       }
+//     } else {
+//       let complement;
+//       if (n % 2 === 0) {
+//         complement = str[mid + 1 - (idx - mid)];
+//       } else {
+//         complement = str[mid - (idx - mid)];
+//       }
+//       recurse(idx + 1, str + valid[complement]);
+//     }
+//   };
+
+//   recurse(0, '');
+//   return result;
+// }
+
+const complements = [['0', '0'], ['1', '1'], ['6', '9'], ['8', '8'], ['9', '6']];
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
 var findStrobogrammatic = function (n) {
-  const valid = { 0: '0', 1: '1', 6: '9', 8: '8', 9: '6' },
-    mid = Math.floor((n - 1) / 2),
-    result = [];
+  const recurse = (n, length) => {
+    if (n === 0) return [''];
+    if (n === 1) return ['0', '1', '8'];
 
-  if (n === 1) result.push('0');
+    middles = recurse(n - 2, length);
 
-  const recurse = (idx, str) => {
-    if (str[0] === '0') return;
-    if (idx >= n) return result.push(str);
+    const result = [];
+    for (let i = 0; i < middles.length; i++) {
+      const middle = middles[i];
 
-    if (idx < mid) {
-      const keys = Object.keys(valid);
-      for (let i = 0; i < keys.length; i++) {
-        recurse(idx + 1, str + keys[i]);
-      }
-    } else if (idx === mid) {
-      if (n % 2 === 0) {
-        const keys = Object.keys(valid);
-        for (let i = 0; i < keys.length; i++) {
-          recurse(idx + 1, str + keys[i]);
-        }
-      } else {
-        const middleKeys = ['0', '1', '8'];
-        for (let i = 0; i < middleKeys.length; i++) {
-          recurse(idx + 1, str + middleKeys[i]);
-        }
-      }
-    } else {
-      let complement;
-      if (n % 2 === 0) {
-        complement = str[mid + 1 - (idx - mid)];
-      } else {
-        complement = str[mid - (idx - mid)];
-      }
-      recurse(idx + 1, str + valid[complement]);
+      if (middle.length !== length - 2) result.push('0' + middle + '0');
+      result.push('1' + middle + '1');
+      result.push('6' + middle + '9');
+      result.push('8' + middle + '8');
+      result.push('9' + middle + '6');
     }
-  };
+    return result;
+  }
+  return recurse(n, n);
+};
 
-  recurse(0, '');
-  return result;
-}
-
-console.log(findStrobogrammatic(1));
-console.log(findStrobogrammatic(2));
-console.log(findStrobogrammatic(3));
+// console.log(findStrobogrammatic(1));
+// console.log(findStrobogrammatic(2));
+// console.log(findStrobogrammatic(3));
 console.log(findStrobogrammatic(4));
