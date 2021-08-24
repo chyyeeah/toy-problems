b43b8a41bd1962c07d06a807de4f7738f6aac727
chyyeeah <wilsoncheah5@gmail.com>
2021-08-24 12:22:44 -0700

    time:O(1). Linked List + Hash Table

--- LRUCache.js
+++ LRUCache.js
@@ -1,10 +1,21 @@
+class ListNode {
+  constructor(key, val, prev, next) {
+    this.key = key !== undefined ? key : null;
+    this.val = val !== undefined ? val : null;
+    this.prev = prev ? prev : null;
+    this.next = next ? next : null;
+  }
+}
+
 /**
  * @param {number} capacity
  */
 var LRUCache = function (capacity) {
-  this._capacity = capacity;
-  this._store = {};
-  this._tracker = [];
+  this.capacity = capacity;
+  this.tail = new ListNode(null, null, this.head, null);
+  this.head = new ListNode(null, null, null, this.tail);
+  this.hashMap = {};
+  this.size = 0;
 };
 
 /**
@@ -12,13 +23,19 @@ var LRUCache = function (capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function (key) {
-  if (this._store[key] !== undefined) {
-    const idx = this._tracker.indexOf(key);
-    this._tracker.splice(idx, 1);
-    this._tracker.unshift(key);
-    return this._store[key];
-  }
-  return -1;
+  const current = this.hashMap[key];
+  if (!current) return -1;
+  const prev = current.prev,
+    next = current.next;
+
+  prev.next = next;
+  next.prev = prev;
+
+  current.prev = this.head;
+  current.next = this.head.next;
+  this.head.next.prev = current;
+  this.head.next = current;
+  return current.val;
 };
 
 /**
@@ -27,16 +44,29 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
-  if (this._store[key] !== undefined) {
-    const idx = this._tracker.indexOf(key);
-    this._tracker.splice(idx, 1);
+  let current;
+  if (this.hashMap[key]) {
+    current = this.hashMap[key];
+    current.val = value;
+    current.next.prev = current.prev;
+    current.prev.next = current.next;
+    current.prev = this.head;
+    current.next = this.head.next;
+  } else {
+    current = new ListNode(key, value, this.head, this.head.next);
+    this.hashMap[key] = current;
+    this.size++;
   }
-  if (this._tracker.length === this._capacity) {
-    const lru = this._tracker.pop();
-    delete this._store[lru];
+  this.head.next.prev = current;
+  this.head.next = current;
+
+  if (this.size > this.capacity) {
+    const last = this.tail.prev;
+    this.tail.prev = last.prev;
+    last.prev.next = this.tail;
+    delete this.hashMap[last.key];
+    this.size--;
   }
-  this._store[key] = value;
-  this._tracker.unshift(key);
 };
 
 /**
@@ -45,3 +75,24 @@ LRUCache.prototype.put = function (key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+
+let obj = new LRUCache(2);
+obj.put(1, 1);
+obj.put(2, 2);
+console.log(obj.get(1));
+obj.put(3, 3);
+console.log(obj.get(2));
+obj.put(4, 4);
+console.log(obj.get(1));
+console.log(obj.get(3));
+console.log(obj.get(4));
+console.log('~~~');
+// console.log(obj);
+obj = new LRUCache(2);
+obj.put(2,1);
+obj.put(1,1);
+obj.put(2,3);
+obj.put(4,1);
+console.log(obj.get(1));
+console.log(obj.get(2));
+// console.log(obj);
