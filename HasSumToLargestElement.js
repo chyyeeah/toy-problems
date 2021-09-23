6abfa7f891243bd222301213bfa9c6687ccb3955
Wilson Cheah <wilsoncheah5@gmail.com>
2021-09-22 21:56:51 -0700

    code challenge

--- HasSumToLargestElement.js
+++ HasSumToLargestElement.js
@@ -1,1 +1,27 @@
-const hasSumToLargestElement = () => {};
+const hasSumToLargestElement = (arr) => {
+  let max = -Infinity, idx;
+  arr.forEach((num, i) => {
+    if (num > max) {
+      max = num;
+      idx = i;
+    }
+  });
+
+  const recurse = (remaining, i) => {
+    if (remaining === 0) return 0;
+    if (remaining < 0) return -1;
+
+    for (let j = i; j < arr.length; j++) {
+      if (j === idx) continue;
+      const result = recurse(remaining - arr[j], j + 1);
+      if (result === 0) return 0;
+    }
+
+    return -1;
+  };
+
+  return recurse(max, 0) === 0 ? true : false;
+};
+
+console.log(hasSumToLargestElement([1,3,5,13,10]));
+console.log(hasSumToLargestElement([1,3,5,10]));
