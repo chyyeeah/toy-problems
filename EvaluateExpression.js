b527f620dc5090fc4e504073e4c29cf6be331f33
chyyeeah <wilsoncheah5@gmail.com>
2021-10-08 23:12:19 -0700

    starter code for question

---
+++ EvaluateExpression.js
@@ -0,0 +1,9 @@
+const diff_ways_to_evaluate_expression = function(input) {
+  result = [];
+  // TODO: Write your code here
+  return result;
+};
+
+
+console.log(`Expression evaluations: ${diff_ways_to_evaluate_expression("1+2*3")}`)
+console.log(`Expression evaluations: ${diff_ways_to_evaluate_expression("2*3-4-5")}`)
