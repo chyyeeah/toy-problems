b6de519dd4a11f839c6893ed4d1bc9ddc411e12c
chyyeeah <wilsoncheah5@gmail.com>
2021-10-14 22:41:30 -0700

    starter code

---
+++ HeightBalancedBinaryTree.js
@@ -0,0 +1,13 @@
+// This is an input class. Do not edit.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function heightBalancedBinaryTree(tree) {
+  // Write your code here.
+  return false;
+}
